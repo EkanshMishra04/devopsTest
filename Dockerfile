e47fbcc808c4075cd4d530ece3c49c2a3c286dd6
ekanshmishra <ekansh.mishra@nagarro.com>
2023-03-09 09:11:09 +0530

    removed repo layer

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-from openjdk
+from openjdk:11
 EXPOSE 8089
 ADD target/DevOpsAssignment-0.0.1-SNAPSHOT.jar DevOpsAssignment-0.0.1-SNAPSHOT.jar
 ENTRYPOINT ["java","-jar","/DevOpsAssignment-0.0.1-SNAPSHOT.jar"]
--- src/main/java/com/nagarro/controller/StudentController.java
+++ src/main/java/com/nagarro/controller/StudentController.java
@@ -1,16 +1,10 @@
 package com.nagarro.controller;
 
-import java.util.List;
-
-import com.nagarro.dto.StudentDto;
 import com.nagarro.model.Student;
 import com.nagarro.service.StudentService;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,18 +16,8 @@ public class StudentController {
 	private StudentService service;
 	
 	@GetMapping("/")
-	public List<Student> findAllStudent() {
+	public Student findAllStudent() {
 		return service.findAllStudents();
-	}
-	@GetMapping("/{id}")
-	public Student findStudentById(@PathVariable("id") Integer id) {
-		return service.findStudentById(id);
-	}
-	
-	@PostMapping("/")
-	public Student saveStudentRecord(@RequestBody StudentDto studentdto) {
-		Student student = service.studentDtoToStudent(studentdto);
-		return service.saveStudentRecord(student);
 	}
 	
 	
